9587a76c1f69963caf556b4c934410c45552addc
Ruben Weijer <ruben.weijer@cloudsters.nl>
2021-06-14 20:17:36 +0200

    oops

--- setup_auditbeat.sh
+++ setup_auditbeat.sh
@@ -5,5 +5,6 @@ cd auditbeat-7.9.3-amd64.deb
 rm auditbeat.yml
 wget https://raw.githubusercontent.com/ProAdmin007/Salt_scripts/main/auditbeat.yml
 
-./auditbeat setup
-./auditbeat -e
+auditbeat setup
+service auditbeat start
+service auditbeat enable

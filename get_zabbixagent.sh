c16538485008f8dab3814bb44ba312f24682aa26
Ruben Weijer <ruben.weijer@cloudsters.nl>
2021-06-14 13:46:22 +0200

    Config fix zabbix

--- get_zabbixagent.sh
+++ get_zabbixagent.sh
@@ -5,5 +5,10 @@ dpkg -i zabbix-release_4.2-1%2Bbionic_all.deb
 apt-get install zabbix-agent
 
 cd /etc/zabbix/
-rm auditbeat.yml
-git pull https://github.com/ProAdmin007/Salt_scripts/blob/main/auditbeat.yml
+zabbix_agentd.conf
+git pull https://github.com/ProAdmin007/Salt_scripts/blob/main/zabbix_agentd.conf
+
+systemctl restart zabbix-agent
+systemctl status zabbix-agent
+ 
+ufw allow 10050/tcp

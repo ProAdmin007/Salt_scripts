cf4f38ce3c0bb39764e287e48eb1d77b926db7a3
Ruben Weijer <ruben.weijer@cloudsters.nl>
2021-06-14 20:14:12 +0200

    Dabian fix

--- setup_auditbeat.sh
+++ setup_auditbeat.sh
@@ -1,6 +1,7 @@
-curl -L -O https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-7.9.3-darwin-x86_64.tar.gz
-tar xzvf auditbeat-7.9.3-darwin-x86_64.tar.gz
-cd auditbeat-7.9.3-darwin-x86_64/
+curl -L -O https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-7.9.3-amd64.deb
+dpkg -i auditbeat-7.9.3-amd64.deb
+
+cd auditbeat-7.9.3-amd64.deb
 rm auditbeat.yml
 wget https://raw.githubusercontent.com/ProAdmin007/Salt_scripts/main/auditbeat.yml
 

6e76799f591bff3e232857fd5ca3a9f12d178d0b
Ruben Weijer <ruben.weijer@cloudsters.nl>
2021-06-14 15:54:23 +0200

    wget

--- setup_auditbeat.sh
+++ setup_auditbeat.sh
@@ -2,7 +2,7 @@ curl -L -O https://artifacts.elastic.co/downloads/beats/auditbeat/auditbeat-7.9.
 tar xzvf auditbeat-7.9.3-darwin-x86_64.tar.gz
 cd auditbeat-7.9.3-darwin-x86_64/
 rm auditbeat.yml
-git clone https://github.com/ProAdmin007/Salt_scripts/blob/main/auditbeat.yml
+wget https://raw.githubusercontent.com/ProAdmin007/Salt_scripts/main/auditbeat.yml
 
 ./auditbeat setup
 ./auditbeat -e

be69583bca4584ebe9b4b6b713d3f879d038f58c
Ruben Weijer <ruben.weijer@cloudsters.nl>
2021-06-14 15:08:43 +0200

    fix

--- get_zabbixagent.sh
+++ get_zabbixagent.sh
@@ -1,6 +1,6 @@
 wget http://repo.zabbix.com/zabbix/4.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.2-1%2Bbionic_all.deb
 
-dpkg -i zabbix-release_4.2-1%2Bbionic_all.deb
+dpkg -i zabbix-release_4.2-1+bionic_all.deb
 
 apt-get install zabbix-agent
 
